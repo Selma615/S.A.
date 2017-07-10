ab375530a515157bd699a7b1e93012dbafaf3589
Selma615 <manar615@icloud.com>
2017-07-10 15:47:57 -0500

    Add files via upload

--- proj01/proj01.py
+++ proj01/proj01.py
@@ -6,5 +6,15 @@
 # Then, it prints a sentence that says when the user will turn 100.
 
 # If you complete extensions, describe your extensions here!
-
-
+Name = raw_input("enter your name: ")
+Birthday = raw_input('did your birthday happen?:')
+Age = raw_input("enter your age:")
+Age = int(Age)
+if Birthday == "no":
+ Age= Age + 1
+else:
+ Age + 0
+birthyear= 2017 - Age
+year = birthyear + 100
+print year
+raw_input("is when you will turn 100! ")
