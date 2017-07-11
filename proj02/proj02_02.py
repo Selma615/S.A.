139373b04128d9cbd6992e6f43ad14915930a7f9
Selma615 <manar615@icloud.com>
2017-07-11 12:53:35 -0500

    Add files via upload

--- proj02/proj02_02.py
+++ proj02/proj02_02.py
@@ -4,10 +4,12 @@
 # proj02_02: Fibonaci Sequence
 
 """
-Asks a user how many Fibonacci numbers to generate and generates them. The Fibonacci 
-sequence is a sequence of numbers where the next number in the sequence is the sum of the 
-previous two numbers in the sequence. The sequence looks like this: 
-1, 1, 2, 3, 5, 8, 13...
-"""
-
+# Asks a user how many Fibonacci numbers to generate and generates them. The Fibonacci
+# sequence is a sequence of numbers where the next number in the sequence is the sum of the
+# previous two numbers in the sequence. The sequence looks like this:
+# 1, 1 .2. 3, 5, 8, 13... """
+While loop_control
+F = raw_input('How many Fibonacci numbers would you like?')
+loop_control = True
+F = int(F)
 
